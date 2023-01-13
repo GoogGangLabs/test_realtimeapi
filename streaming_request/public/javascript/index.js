1dba371ecd66c9e7682f193a8846a73322d1f9b7
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-01-14 02:43:58 +0900

    #5 [Add] MediaStream으로 웹캠 Stream 불러오는 코드 추가

--- streaming_request/public/javascript/index.js
+++ streaming_request/public/javascript/index.js
@@ -1,32 +1,53 @@
-const start = () => {
-  const mediaDevices = navigator.mediaDevices;
-  const originalVideo = document.getElementById('originalVideo');
-  const mediaPipeVideo = document.getElementById('mediaPipeVideo');
+let stream = null;
+let flag = false;
 
-  if (!mediaDevices || !mediaDevices.getUserMedia) {
-    alert('웹캠 사용이 가능한 디바이스로 접속해주세요.');
-    return;
-  }
+const loadVideo = async () => {
+  const originCanvas = document.getElementById('origin-canvas');
+  const changedCanvas = document.getElementById('changed-canvas');
+  const originContext = originCanvas.getContext('2d');
+  const changedContext = changedCanvas.getContext('2d');
+  const inputVideo = document.getElementById('input-video');
 
-  mediaDevices
+  await navigator.mediaDevices
     .getUserMedia({ video: true })
-    .then((stream) => {
-      originalVideo.srcObject = stream;
-      mediaPipeVideo.srcObject = stream;
+    .then((media) => {
+      const inputVideo = document.getElementById('input-video');
+
+      stream = media;
+      inputVideo.srcObject = media;
+      inputVideo.play();
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
+
+  flag = true;
+
+  const loop = async () => {
+    originContext.save();
+    changedContext.save();
+
+    originContext.drawImage(inputVideo, 0, 0, originCanvas.width, originCanvas.height);
+    changedContext.drawImage(inputVideo, 0, 0, changedCanvas.width, changedCanvas.height);
+
+    originContext.restore();
+    changedContext.restore();
+
+    if (flag) requestAnimationFrame(loop);
+  };
+
+  await loop();
 };
 
 const end = () => {
-  const originalVideo = document.getElementById('originalVideo');
-  const mediaPipeVideo = document.getElementById('mediaPipeVideo');
+  const inputVideo = document.getElementById('input-video');
 
-  if (!originalVideo.srcObject) return;
+  if (!flag) return;
 
-  originalVideo.srcObject.getTracks()[0].stop();
+  flag = false;
+  stream.getTracks()[0].stop();
+  stream = null;
+  inputVideo.srcObject = null;
+};
 
-  originalVideo.srcObject = null;
-  mediaPipeVideo.srcObject = null;
+window.onload = () => {
+  connectStreamRequest();
 };
