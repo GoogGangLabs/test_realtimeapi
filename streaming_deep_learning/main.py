1ada93a9ce49aa1625d29b52f4b2bdb701ff5231
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-01-15 22:12:36 +0900

    #8 [Add] Redis <-> Pythone 파이프라인 구축

--- streaming_deep_learning/main.py
+++ streaming_deep_learning/main.py
@@ -1,47 +1,28 @@
-import cv2
-import mediapipe as mp
+# import threading
+import redis
+import json
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+import hands
 
-cap = cv2.VideoCapture(0)
+conn = redis.Redis(host='localhost', port=6379, db=0)
 
-with mp_hands.Hands(
-  max_num_hands = 1,
-  min_detection_confidence = 0.5,
-  min_tracking_confidence = 0.5
-) as hands:
+def pipeline():
+  subscribe(conn.pubsub())
 
-  while cap.isOpened():
-    success, image = cap.read()
-    
-    if not success:
-      continue
+def subscribe(subscriber):
+  subscriber.subscribe('STREAM_PREPROCESS')
 
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  while True:
+    message = subscriber.get_message()
+    if message is not None and message['type'] == 'message':
+      publish(message['data'].decode())
+      # threading.Thread(target=publish, args=(message['data'].decode())).start()
 
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        finger1 = int(hand_landmarks.landmark[4].x * 100)
-        finger2 = int(hand_landmarks.landmark[8].x * 100)
-        dist = abs(finger1 - finger2)
+def publish(message):
+  data = eval(message)
+  result = hands.process(data['data']['frame'])
+  dict = {'sessionId': data['data']['sessionId'], 'result': result}
+  conn.publish(channel='STREAM_POSTPROCESS', message=json.dumps(dict).encode())
 
-        cv2.putText(
-          image,
-          text = 'f1=%d f2=%d dist=%d ' % (finger1,finger2,dist), org=(10, 30),
-          fontFace = cv2.FONT_HERSHEY_SIMPLEX,
-          fontScale= 1,
-          color = 255,
-          thickness= 3
-        )
-
-        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-    cv2.imshow('image', image)
-
-    if cv2.waitKey(1) == ord('q'):
-      break
-
-cap.release()
+if __name__ == '__main__':
+  pipeline()
