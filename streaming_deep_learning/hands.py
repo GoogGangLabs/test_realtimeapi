08fb3cd79cd571fc2006c7b0b735fe239f073a17
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-01-17 07:02:30 +0900

    #9 [Change] Holistic 좌표 반환 로직 작성

--- streaming_deep_learning/hands.py
+++ streaming_deep_learning/hands.py
@@ -3,34 +3,41 @@ import numpy
 import mediapipe as mp
 import base64
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+mp_holistic = mp.solutions.holistic
 
 def toImage(base64Data):
   imageData = base64.b64decode(base64Data)
   np_data = numpy.fromstring(imageData, numpy.uint8)
   return cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
 
+def get_landmark_list(landmarks):
+  if landmarks is None:
+    return []
+
+  landmark_list = []
+
+  for _, landmark in enumerate(landmarks.landmark):
+    element = {}
+
+    element['x'] = landmark.x
+    element['y'] = landmark.y
+    element['z'] = landmark.z
+    element['visibility'] = landmark.visibility
+
+    landmark_list.append(element)
+  
+  return landmark_list
+
 def process(base64Data):
   image = toImage(base64Data)
 
-  with mp_hands.Hands(
-    static_image_mode = True,
-    max_num_hands = 1,
-    min_detection_confidence = 0.5
-  ) as hands:
+  with mp_holistic.Holistic(static_image_mode=True) as holistic:
 
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-
-        mp_drawing.draw_landmarks(
-          image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS
-        )
-
-    _, buffer = cv2.imencode('.jpeg', image)
-
-    return str(base64.b64encode(buffer), 'utf-8')
+    return {
+      'left_hand': get_landmark_list(results.left_hand_landmarks),
+      'right_hand': get_landmark_list(results.right_hand_landmarks),
+      'pose': get_landmark_list(results.pose_landmarks),
+      'face': get_landmark_list(results.face_landmarks)
+    }
