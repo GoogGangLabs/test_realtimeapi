d39735d5022fc51233169de92d3f696bd6b77f20
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-01-19 04:10:27 +0900

    #9 [Add] pose, hand, face 결과 drawingUtils로 적용

--- streaming_preprocess/public/javascript/socket.js
+++ streaming_preprocess/public/javascript/socket.js
@@ -4,6 +4,24 @@ const socket = {
   postProcess: undefined,
 };
 
+function connect(ctx, connectors) {
+  const canvas = ctx.canvas;
+
+  for (const connector of connectors) {
+    const from = connector[0];
+    const to = connector[1];
+    if (from && to) {
+      if (from.visibility && to.visibility && (from.visibility < 0.1 || to.visibility < 0.1)) {
+        continue;
+      }
+      ctx.beginPath();
+      ctx.moveTo(from.x * canvas.width, from.y * canvas.height);
+      ctx.lineTo(to.x * canvas.width, to.y * canvas.height);
+      ctx.stroke();
+    }
+  }
+}
+
 const connectStreamPreProcess = () => {
   socket.preProcess = io(`${serverHost}:4000`);
   streamPreProcessOn();
@@ -30,23 +48,40 @@ const streamPostProcessOn = () => {
   socket.postProcess.on('server:postprocess:stream', async (results) => {
     const base64Data = bufferQueue.pop();
 
-    console.log(results);
     image.onload = () => {
       changedContext.save();
       changedContext.drawImage(image, 0, 0, changedCanvas.width, changedCanvas.height);
       originContext.drawImage(image, 0, 0);
 
       changedContext.globalCompositeOperation = 'source-over';
-      drawConnectors(changedContext, results.pose, POSE_CONNECTIONS, { color: '#00FF00', lineWidth: 0.5 });
-      drawLandmarks(changedContext, results.pose, { color: '#FF0000', lineWidth: 1 });
-      drawConnectors(changedContext, results.face, FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 0.5 });
-      drawConnectors(changedContext, results.left_hand, HAND_CONNECTIONS, { color: '#CC0000', lineWidth: 0.5 });
-      drawLandmarks(changedContext, results.left_hand, { color: '#00FF00', lineWidth: 0.5 });
-      drawConnectors(changedContext, results.right_hand, HAND_CONNECTIONS, { color: '#00CC00', lineWidth: 0.5 });
-      drawLandmarks(changedContext, results.right_hand, { color: '#FF0000', lineWidth: 0.5 });
+
+      if (results.pose.length > 0) {
+        for (let i = 0; i < 23; i++) {
+          if (i > 10 && i < 17) continue;
+          results.pose[i] = [0, 0, 0, 0];
+        }
+      }
+
+      /* Face */
+      drawConnectors(changedContext, results.face, FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_RIGHT_EYE, { color: 'rgb(0,217,231)', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_RIGHT_EYEBROW, { color: 'rgb(0,217,231)', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_LEFT_EYE, { color: 'rgb(255,138,0)', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_LEFT_EYEBROW, { color: 'rgb(255,138,0)', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_FACE_OVAL, { color: '#E0E0E0', lineWidth: 1 });
+      drawConnectors(changedContext, results.face, FACEMESH_LIPS, { color: '#E0E0E0', lineWidth: 1 });
+
+      /* Pose */
+      drawConnectors(changedContext, results.pose, POSE_CONNECTIONS, { color: 'white', lineWidth: 2 });
+
+      /* Left Hand */
+      drawConnectors(changedContext, results.left_hand, HAND_CONNECTIONS, { color: 'rgb(255,138,0)', lineWidth: 1.5 });
+
+      /* Right Hand */
+      drawConnectors(changedContext, results.right_hand, HAND_CONNECTIONS, { color: 'rgb(0,217,231)', lineWidth: 2 });
 
       changedContext.restore();
     };
-    image.src = 'data:image/png;base64,' + base64Data;
+    image.src = 'data:image/jpeg;base64,' + base64Data;
   });
 };
