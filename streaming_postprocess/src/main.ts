1dae97296cac1dc3109f3f55d20fc92389b3466a
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-01-24 05:31:20 +0900

    #10 [Change] 기본 환경 설정

--- streaming_postprocess/src/main.ts
+++ streaming_postprocess/src/main.ts
@@ -7,8 +7,8 @@ const bootstrap = async () => {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.REDIS,
     options: {
-      host: process.env.REDIS_HOST,
-      port: parseInt(process.env.REDIS_PORT, 10),
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT, 10) || 6379,
     },
   });
 
