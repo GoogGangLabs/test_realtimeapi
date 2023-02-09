3237b9a1b287fedb744f634aa0a843295d28f007
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-02-09 21:58:52 +0900

    #16 [Change] Redis -> RabbitMQ 딥러닝 서버 Pub/Sub 전환

--- streaming_deep_learning/main.py
+++ streaming_deep_learning/main.py
@@ -1,31 +1,36 @@
 import os
-import redis
 import json
 import asyncio
+import pika
 
 import holistic
 
-host = os.environ.get('REDIS_HOST') if os.environ.get('REDIS_HOST') is not None else 'localhost'
-port = int(os.environ.get('REDIS_PORT')) if os.environ.get('REDIS_PORT') is not None else 6379
+SUB_QUEUE = os.environ.get('RABBITMQ_PRE_QUEUE')
+PUB_EXCHANGE = os.environ.get('RABBITMQ_POST_EXCHANGE')
 
-conn = redis.Redis(host, port, db=0)
+host = os.environ.get('RABBITMQ_HOST')
+port = os.environ.get('RABBITMQ_PORT')
+username = os.environ.get('RABBITMQ_USERNAME')
+password = os.environ.get('RABBITMQ_PASSWORD')
+
+credentials = pika.PlainCredentials(username=username, password=password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port, credentials=credentials))
+channel = connection.channel()
 
 def pipeline():
-  subscribe(conn.pubsub())
+  channel.queue_declare(queue=SUB_QUEUE, durable=True, auto_delete=False)
+  channel.exchange_declare(exchange=PUB_EXCHANGE, exchange_type='fanout', durable=True, auto_delete=False)
+  channel.basic_consume(queue=SUB_QUEUE, on_message_callback=subscribe, auto_ack=True)
+  channel.start_consuming()
 
-def subscribe(subscriber):
-  subscriber.subscribe('STREAM_PREPROCESS')
-
-  while True:
-    message = subscriber.get_message()
-    if message is not None and message['type'] == 'message':
-      asyncio.run(publish(message['data'].decode()))
+def subscribe(_, __, ___, body):
+  asyncio.run(publish(body))
 
 async def publish(message):
   data = eval(message)
-  result = holistic.process(data['data']['frame'])
-  dict = {'sessionId': data['data']['sessionId'], 'result': result}
-  conn.publish(channel='STREAM_POSTPROCESS', message=json.dumps(dict).encode())
+  result = holistic.process(data['frame'])
+  dict = { 'sessionId': data['sessionId'], 'result': result }
+  channel.basic_publish(exchange=PUB_EXCHANGE, routing_key='', body=json.dumps(dict).encode())
 
 if __name__ == '__main__':
   pipeline()
