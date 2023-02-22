3eee5ade5e51e63b19b8d2ec7aba49109a075c9a
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2023-02-22 19:43:12 +0900

    #21 [Change] Inference 서버 모듈 load 방식 변경

--- streaming_deep_learning/main.py
+++ streaming_deep_learning/main.py
@@ -2,8 +2,11 @@ import os
 import json
 import asyncio
 import aio_pika
-
-import holistic
+import cv2
+import numpy
+import mediapipe as mp
+import time
+import math
 
 SUB_QUEUE = os.environ.get('RABBITMQ_PRE_QUEUE')
 PUB_EXCHANGE = os.environ.get('RABBITMQ_POST_EXCHANGE')
@@ -12,6 +15,30 @@ host = os.environ.get('RABBITMQ_HOST')
 port = os.environ.get('RABBITMQ_PORT')
 username = os.environ.get('RABBITMQ_USERNAME')
 password = os.environ.get('RABBITMQ_PASSWORD')
+
+def get_landmark_list(landmarks):
+  landmark_list = []
+
+  if landmarks is None:
+    return landmark_list
+
+  for _, landmark in enumerate(landmarks.landmark):
+    element = {}
+    element['x'] = landmark.x
+    element['y'] = landmark.y
+    element['z'] = landmark.z
+
+    if landmark.visibility > 0.0:
+      element['visibility'] = landmark.visibility
+
+    landmark_list.append(element)
+
+  return landmark_list
+
+def checkTime(timestamp, step):
+  serverTime = math.floor(time.time() * 1000)
+  step.append(serverTime - timestamp[-1])
+  timestamp.append(serverTime)
 
 async def pipeline(loop):
   connection = await aio_pika.connect_robust(
@@ -27,13 +54,38 @@ async def pipeline(loop):
   queue = await channel.declare_queue(SUB_QUEUE, durable=True)
   exchange = await channel.declare_exchange(PUB_EXCHANGE, type=aio_pika.ExchangeType.FANOUT, durable=True)
 
-  async with queue.iterator() as queue_iter:
-    async for message in queue_iter:
-      async with message.process():
-        data = json.loads(message.body.decode())
-        result = holistic.process(data['sequence'], data['frame'])
-        dict = { 'sessionId': data['sessionId'], 'sequence': data['sequence'], 'result': result }
-        await exchange.publish(aio_pika.Message(json.dumps(dict).encode()), routing_key='')
+  mp_holistic = mp.solutions.holistic
+  with mp_holistic.Holistic(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as holistic:
+    async with queue.iterator() as queue_iter:
+      async for message in queue_iter:
+        async with message.process():
+          data = json.loads(message.body.decode())
+
+          checkTime(data['timestamp'], data['step'])
+          
+          np_data = numpy.asarray(data['frame']['data'], numpy.uint8)
+          image = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+          result = holistic.process(image[..., ::-1])
+          resultJSON = {
+            'left_hand': get_landmark_list(result.left_hand_landmarks),
+            'right_hand': get_landmark_list(result.right_hand_landmarks),
+            'pose': get_landmark_list(result.pose_landmarks),
+            'face': get_landmark_list(result.face_landmarks)
+          }
+
+          checkTime(data['timestamp'], data['step'])
+
+          dict = {
+            'sessionId': data['sessionId'],
+            'sequence': data['sequence'],
+            'result': resultJSON,
+            'startedAt': data['startedAt'],
+            'timestamp': data['timestamp'],
+            'step': data['step']
+          }
+          await exchange.publish(aio_pika.Message(json.dumps(dict).encode()), routing_key='')
 
 if __name__ == '__main__':
   loop = asyncio.get_event_loop()
